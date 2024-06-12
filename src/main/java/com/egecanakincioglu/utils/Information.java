bfe86296aa8ccdf244c97c044e48af96546bd4b9
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-06-12 03:37:30 +0300

    Core editted and Logger created

--- src/main/java/com/egecanakincioglu/utils/Information.java
+++ src/main/java/com/egecanakincioglu/utils/Information.java
@@ -1,7 +1,7 @@
 package com.egecanakincioglu.utils;
 
 public class Information {
-    public static final String GITHUB_URL = "";
+    public static final String GITHUB_URL = "https://github.com/egecanakincioglu/auth";
     public static final String USER_AGENT = "";
     public static final String VERSION = "@version@";
 }
