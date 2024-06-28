0445d56d8edf082875f156fd42bc32cb20e22231
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-06-28 13:04:40 +0300

    Command Builder updated and added option types

--- src/main/java/com/egecanakincioglu/handlers/builders/CommandBuilder.java
+++ src/main/java/com/egecanakincioglu/handlers/builders/CommandBuilder.java
@@ -1,10 +1,18 @@
 package com.egecanakincioglu.handlers.builders;
 
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class CommandBuilder {
     private final String name;
     private final String description;
+    private final List<OptionData> options = new ArrayList<>();
 
     public CommandBuilder(String name, String description) {
         this.name = name;
@@ -19,5 +27,19 @@ public abstract class CommandBuilder {
         return description;
     }
 
+    protected void addOption(OptionType type, String name, String description, boolean required) {
+        this.options.add(new OptionData(type, name, description).setRequired(required));
+    }
+
+    public List<OptionData> getOptions() {
+        return options;
+    }
+
     public abstract void execute(SlashCommandInteractionEvent event);
+
+    public SlashCommandData getSlashCommandData() {
+        SlashCommandData commandData = Commands.slash(name, description);
+        commandData.addOptions(options);
+        return commandData;
+    }
 }
