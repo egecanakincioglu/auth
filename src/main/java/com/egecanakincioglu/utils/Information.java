4f6b2b8476f3f9971f9f24c487750cf93d6f7396
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-06-28 13:05:02 +0300

    Information removed

--- src/main/java/com/egecanakincioglu/utils/Information.java
+++
@@ -1,7 +0,0 @@
-package com.egecanakincioglu.utils;
-
-public class Information {
-    public static final String GITHUB_URL = "https://github.com/egecanakincioglu/auth";
-    public static final String USER_AGENT = "";
-    public static final String VERSION = "v1.0";
-}
